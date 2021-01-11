b4921f1e78406235d6b0cb57fd0a4dbd6a69194b
SoCuul <63339559+SoCuul@users.noreply.github.com>
2021-01-10 16:20:23 -0800

    Fix the accept/decline commands

--- commands/accept.js
+++ commands/accept.js
@@ -1,16 +1,22 @@
 module.exports = {
-	run(client, message) {
+	run(client, message, args) {
 		if(message.guild.id === client.config.guildID){
 			const Discord = require("discord.js");
 			const axios = require("axios");
+			function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); } 
 			const member = message.guild.members.cache.get(args[0])
 			let notes = args.slice(1).join(' ')
-			let MemberRole = message.member.guild.roles.cache.find(r => r.name === client.config.verifiedRoleName);
-			if(!MemberRole) return message.channel.send(`❌ The \`${client.config.verifiedRoleName}\` role could not be found.`)
-			function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); } 
-			if(message.member.roles.cache.has(client.config.staffRoleName)){
+			//Staff Role
+			let staffRole = message.member.guild.roles.cache.find(r => r.name === client.config.staffRoleName);
+			if(!staffRole) return message.channel.send(`❌ The selected staff role could not be found.`)
+			//Verified Role
+			let memberRole = message.member.guild.roles.cache.find(r => r.name === client.config.verifiedRoleName);
+			if(!memberRole) return message.channel.send(`❌ The selected verified role could not be found.`)
+
+			if(message.member.roles.cache.find(r => r.name === client.config.staffRoleName)){
 				if(!args[0]) return message.reply(`Please enter a discord user id`)
 				if(!isNumber(args[0])) return message.reply(`Please enter a discord user id`)
+				if(!member) return message.reply(`Please make sure that the user is in this server`)
 
 				message.react('✅').catch(error => { message.channel.send(`❌ There was an error adding the reaction`) })
 			
@@ -26,9 +32,9 @@ module.exports = {
 						if(notes){
 							embed.addField('Notes', notes)
 						}
-						member.send(embed).catch(error => { message.channel.send(`❌ There was an error dming <@${args[0]}`) })
+						member.send(embed).catch(error => { message.channel.send(`❌ There was an error dming <@${args[0]}>`) })
 						
-						member.roles.add(MemberRole).catch(error => { message.channel.send(`❌ There was an error adding the \`${client.config.verifiedRoleName}\` role.`) })
+						member.roles.add(memberRole).catch(error => { message.channel.send(`❌ There was an error adding the \`${client.config.verifiedRoleName}\` role.`) })
 
 						member.setNickname(client.config.verifiedPrefix + response.data.robloxUsername, `Verification Accepted`).catch(error => { message.channel.send(`❌ There was an error changing the user's nickname.`) })
 
@@ -42,7 +48,7 @@ module.exports = {
 					message.channel.send(`❌ I couldnt find a Roblox account linked to <@${args[0]}>, or there was an error.`)
 				})
 			}else{
-			message.reply(`❌ You have have the \`${client.config.staffRoleName}\` to use this command`)
+			message.reply(`❌ You have have the \`${client.config.staffRoleName}\` role to use this command`)
 			}
 		}
 	}
--- commands/decline.js
+++ commands/decline.js
@@ -1,12 +1,16 @@
 module.exports = {
-	run(client, message) {
+	run(client, message, args) {
 		if(message.guild.id === client.config.guildID){
 			const Discord = require("discord.js");
 			const axios = require("axios");
+			function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); } 
 			const member = message.guild.members.cache.get(args[0])
 			let reason = args.slice(1).join(' ') || 'No reason provided'
-			function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); } 
-			if(message.member.roles.cache.has(client.config.staffRoleName)){
+			//Staff Role
+			let staffRole = message.member.guild.roles.cache.find(r => r.name === client.config.staffRoleName);
+			if(!staffRole) return message.channel.send(`❌ The selected staff role could not be found.`)
+			
+			if(message.member.roles.cache.find(r => r.name === client.config.staffRoleName)){
 				if(!args[0]) return message.reply(`Please enter a discord user id`)
 				if(!isNumber(args[0])) return message.reply(`Please enter a discord user id`)
 				if(!member) return message.reply(`Please make sure that the user is in this server`)
@@ -15,10 +19,10 @@ module.exports = {
 
 				const embed = new Discord.MessageEmbed()
 				.setTitle(client.config.botName)
-				.setColor(declinedVerificationEmbedColor)
+				.setColor(client.config.declinedVerificationEmbedColor)
 				.setDescription(`Your verification has been declined.`)
 				.addField('Reason', reason)
-				member.send(embed).catch(error => { message.channel.send(`❌ There was an error dming <@${args[0]}`) })
+				member.send(embed).catch(error => { message.channel.send(`❌ There was an error dming <@${args[0]}>`) })
 
 				member.kick('Verification Declined')
 				.then(() => {
@@ -29,7 +33,7 @@ module.exports = {
 					console.error(err);
 			  	})
 			}else{
-				message.reply(`❌ You have have the \`${client.config.staffRoleName}\` to use this command`)
+				message.reply(`❌ You have have the \`${client.config.staffRoleName}\` role to use this command`)
 			}
 		}
 	}
