6820ab856ed4763147d820ac88ac064c42e6ed3e
HalfDecentAnimator <81921403+HalfDecentAnimator@users.noreply.github.com>
2022-01-03 05:57:31 +0000

    Fix error output.
    Changes made;
    
    - Make errors write to STDERR rather than STDOUT
    - Make errors exit with code 1 rather than code 0

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.sendError = function (input) {
     if(!input) return
     
     return new Discord.MessageEmbed()
-    .setColor('RED')
+    .setColor('#FF0000')
     .setTitle('Error')
     .setDescription(input)
     .setFooter(client.user.username, client.user.avatarURL({ dynamic: true }))
@@ -45,106 +45,106 @@ if(fs.existsSync('./modules.js')){
 	if(client.config){
         //Presence
         if (typeof client.config.presence !== 'object' || Array.isArray(client.config.presence)) {
-            console.log('[Config Error] "presence" must be an object')
-            process.exit(0)
+            console.error('[Config Error] "presence" must be an object')
+            process.exit(1)
         }
         //Presence: Activities
         if (typeof client.config.presence.activities !== 'object' || !Array.isArray(client.config.presence.activities)) {
-            console.log('[Config Error] "presence.activities" must be an array')
-            process.exit(0)
+            console.error('[Config Error] "presence.activities" must be an array')
+            process.exit(1)
         }
         //Presence: Switch Activity Interval
         if (typeof client.config.presence.switchActivityInterval !== 'number') {
-            console.log('[Config Error] "presence.switchActivityInterval" must be an number')
-            process.exit(0)
+            console.error('[Config Error] "presence.switchActivityInterval" must be an number')
+            process.exit(1)
         }
         //Presence: Status
         if (typeof client.config.presence.status !== 'string') {
-            console.log('[Config Error] "presence.status" must be an string')
-            process.exit(0)
+            console.error('[Config Error] "presence.status" must be an string')
+            process.exit(1)
         }
 
         //Info
         if (typeof client.config.info !== 'object' || Array.isArray(client.config.info)) {
-            console.log('[Config Error] "info" must be an object')
-            process.exit(0)
+            console.error('[Config Error] "info" must be an object')
+            process.exit(1)
         }
         //Info: Guild ID
         if (typeof client.config.info.guildID !== 'string') {
-            console.log('[Config Error] "info.guildID" must be a string')
-            process.exit(0)
+            console.error('[Config Error] "info.guildID" must be a string')
+            process.exit(1)
         }
         //Info: Channel ID
         if (typeof client.config.info.channelID !== 'string') {
-            console.log('[Config Error] "info.channelID" must be a string')
-            process.exit(0)
+            console.error('[Config Error] "info.channelID" must be a string')
+            process.exit(1)
         }
         //Info: Include Ban Button
         if (typeof client.config.info.includeBanButton !== 'boolean') {
-            console.log('[Config Error] "info.includeBanButton" must be a boolean')
-            process.exit(0)
+            console.error('[Config Error] "info.includeBanButton" must be a boolean')
+            process.exit(1)
         }
     
         //Verified
         if (typeof client.config.verified !== 'object' || Array.isArray(client.config.verified)) {
-            console.log('[Config Error] "verified" must be an object')
-            process.exit(0)
+            console.error('[Config Error] "verified" must be an object')
+            process.exit(1)
         }
         //Verified: Notify User
         if (typeof client.config.verified.notifyUser !== 'boolean') {
-            console.log('[Config Error] "verified.notifyUser" must be a boolean')
-            process.exit(0)
+            console.error('[Config Error] "verified.notifyUser" must be a boolean')
+            process.exit(1)
         }
         //Verified: Set Nickname Toggle
         if (typeof client.config.verified.setNickname !== 'boolean') {
-            console.log('[Config Error] "verified.setNickname" must be a boolean')
-            process.exit(0)
+            console.error('[Config Error] "verified.setNickname" must be a boolean')
+            process.exit(1)
         }
         //Verified: Set Role Toggle
         if (typeof client.config.verified.setRole !== 'boolean') {
-            console.log('[Config Error] "verified.setRole" must be a boolean')
-            process.exit(0)
+            console.error('[Config Error] "verified.setRole" must be a boolean')
+            process.exit(1)
         }
         //Verified: Role ID
         if (typeof client.config.verified.setRole === true && typeof client.config.verified.roleID !== 'string') {
-            console.log('[Config Error] "verified.roleID" must be a string')
-            process.exit(0)
+            console.error('[Config Error] "verified.roleID" must be a string')
+            process.exit(1)
         }
         //Presence: Nickname prefix
         if (typeof client.config.verified.setNickname === true && client.config.verified.nicknamePrefix !== 'string') {
-            console.log('[Config Error] "verified.nicknamePrefix" must be a string')
-            process.exit(0)
+            console.error('[Config Error] "verified.nicknamePrefix" must be a string')
+            process.exit(1)
         }
     
         //Colours
         if (typeof client.config.colours !== 'object' || Array.isArray(client.config.colours)) {
-            console.log('[Config Error] "colours" must be an object')
-            process.exit(0)
+            console.error('[Config Error] "colours" must be an object')
+            process.exit(1)
         }
         //Colours: Verification Embed
         if (!await client.validColour(client.config.colours.verificationEmbed)) {
-            console.log('[Config Error] "colours.verificationEmbed" must be a valid colour')
-            process.exit(0)
+            console.error('[Config Error] "colours.verificationEmbed" must be a valid colour')
+            process.exit(1)
         }
         //Colours: Invalid Verification Embed
         if (!await client.validColour(client.config.colours.invalidVerificationEmbed)) {
-            console.log('[Config Error] "colours.invalidVerificationEmbed" must be a valid colour')
-            process.exit(0)
+            console.error('[Config Error] "colours.invalidVerificationEmbed" must be a valid colour')
+            process.exit(1)
         }
         //Colours: Success Embed
         if (!await client.validColour(client.config.colours.successEmbed)) {
-            console.log('[Config Error] "colours.successEmbed" must be a valid colour')
-            process.exit(0)
+            console.error('[Config Error] "colours.successEmbed" must be a valid colour')
+            process.exit(1)
         }
         //Colours: Failure Embed
         if (!await client.validColour(client.config.colours.failureEmbed)) {
-            console.log('[Config Error] "colours.failureEmbed" must be a valid colour')
-            process.exit(0)
+            console.error('[Config Error] "colours.failureEmbed" must be a valid colour')
+            process.exit(1)
         }
         //Colours: Ban Embed
         if (!await client.validColour(client.config.colours.banEmbed)) {
-            console.log('[Config Error] "colours.banEmbed" must be a valid colour')
-            process.exit(0)
+            console.error('[Config Error] "colours.banEmbed" must be a valid colour')
+            process.exit(1)
         }
     }
 })()
@@ -167,6 +167,6 @@ try{
     client.login(process.env.TOKEN)
 }
 catch(error){
-    console.log('[Error] Could not login. Please make sure the token is valid.')
+    console.error('[Error] Could not login. Please make sure the token is valid.')
     process.exit(1)
 }
